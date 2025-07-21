1ac3bed71a213bd0eeb26f6535ae3afbbdb26fe3
Marie Rivier <marie.rivier23@gmail.com>
2025-07-21 16:38:54 +0200

    add photo bouteilles défaut selon type pagination débutée

--- src/Controller/Admin/BouteillesCrudController.php
+++ src/Controller/Admin/BouteillesCrudController.php
@@ -1,8 +1,12 @@
 <?php
-
 namespace App\Controller\Admin;
 
 use App\Entity\Bouteilles;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class BouteillesCrudController extends AbstractCrudController
@@ -10,5 +14,24 @@ class BouteillesCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Bouteilles::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('nom'),
+            IntegerField::new('millesime'),
+            TextField::new('cepage'),
+            TextField::new('type'),
+            TextField::new('pays'),
+            TextField::new('region'),
+            TextareaField::new('description'),
+            ImageField::new('image_name')
+                ->setBasePath('uploads/images/')
+                ->setUploadDir('public/uploads/images/')
+                ->setUploadedFileNamePattern('[slug]-[timestamp].[extension]')
+                ->setRequired(false),
+            DateTimeField::new('updated_at')->hideOnForm(),
+        ];
     }
 }
--- src/Controller/BouteillesController.php
+++ src/Controller/BouteillesController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\BouteillesRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class BouteillesController extends AbstractController
 {
     #[Route('/bouteilles', name: 'bouteilles')]
-    public function index(): Response
+    public function index(Request $request, PaginatorInterface $paginator, BouteillesRepository $vinRepository): Response
     {
+        $query = $vinRepository->createQueryBuilder('v')->getQuery();
+
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), // page actuelle, par défaut 1
+            18 // nombre de bouteilles par page
+        );
+
         return $this->render('bouteilles/bouteilles.html.twig', [
-            'controller_name' => 'BouteillesController',
+            'bouteilles' => $pagination,
         ]);
     }
 }
